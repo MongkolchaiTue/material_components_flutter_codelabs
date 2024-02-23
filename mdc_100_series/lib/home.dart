8d62d423898311a1e0a06a4f280035336c2ef1fe
tue.it <tue.it@bkk.paragon-worldwide.com>
2024-02-23 14:52:41 +0700

    TODO: Make a collection of cards (102)

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -61,38 +61,46 @@ class HomePage extends StatelessWidget {
               }),
         ],
       ),
+      // TODO: Add a grid view (102)
       body: GridView.count(
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
         // TODO: Build a grid of cards (102)
-        children: [
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    children: [
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        children: _buildGridCards(10),
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
     );
   }
+}
+
+// TODO: Make a collection of cards (102)
+List<Card> _buildGridCards(int count) {
+  List<Card> cards = List.generate(count, (int index) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 18.0 / 11.0,
+            child: Image.asset('assets/diamond.png'),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+            child: Column(
+              children: [
+                Text('Title'),
+                SizedBox(height: 8.0),
+                Text('Secondary Text'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  });
+
+  return cards;
 }
